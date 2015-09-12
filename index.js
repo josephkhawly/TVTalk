a4daf223d667c60db8fc5dac5fbc14575f10cfa0
Joseph Khawly <joseph.khawly@gmail.com>
2015-09-12 12:11:52 -0400

    Basic chat works, UI is adequate.

---
+++ index.js
@@ -0,0 +1,22 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(4005, function(){
+  console.log('listening on 4005');
+});
+
+// var server = app.listen(4005);
+// server.listen(4005, function() {
+//     console.log('Server listening at http://localhost:4005/');
+// });
